3c972098bbc50bf4772d9efed86372e4c2fb968e
Eric <epw@google.com>
2023-10-30 21:25:59 -0400

    Refactoring setup to handle permissions errors better

--- init-db.sh
+++ init-db.sh
@@ -4,4 +4,5 @@ DB=/var/local/eric/hermod/db.sqlite
 mkdir -p `dirname $DB`
 rm -f "$DB"
 sqlite3 "$DB" < schema.sql
+chmod g+w `dirname "$DB"`
 chmod g+w "$DB"
---
+++ init.sh
@@ -0,0 +1,4 @@
+./init-db.sh
+./setup-ws.sh
+mkdir -p /var/local/eric/hermod/listeners
+chmod -R g+w /var/local/eric/hermod/listeners
